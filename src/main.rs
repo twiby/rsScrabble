c61a5bd740b6ae11b8c8224d2bb0786ddfd77f6c
twiby <t.balenbois@gmail.com>
2022-11-24 18:20:04 +0100

    Add main methods of tree building and navigating.$' 'TODO: sort children

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,23 @@ mod str_tree;
 use str_tree::StrTree;
 
 fn main() {
-        let _tree = StrTree::init();
-        
+        let mut _tree = StrTree::init();
+
+
+        _tree.add_word("arbre");
+        _tree.add_word("ame");
+
+        println!("{}, {}", 'a', _tree.check_child('a'));
+        println!("{}, {}", 'r', _tree.check_child('r'));
+        println!("{}, {}", 'b', _tree.check_child('b'));
+        println!("{}, {}", 'r', _tree.check_child('r'));
+        println!("{}, {}", 'e', _tree.check_child('e'));
+
+        let _node = _tree.get_child('a').unwrap();
+        println!("\n");
+        println!("{}, {}", 'r', _node.check_child('r'));
+        println!("{}, {}", 'm', _node.check_child('m'));
+        println!("{}, {}", 'b', _node.check_child('b'));
+
         println!("Hello, world!");
 }
--- src/str_tree/mod.rs
+++ src/str_tree/mod.rs
@@ -1,19 +1,83 @@
 
-pub struct StrTree<'a> {
+pub struct StrTree {
 	data: Option<char>,
 	nb_letters: u8,
 	is_word: bool,
-	parent: Option<&'a StrTree<'a>>,
-	children: Vec<&'a StrTree<'a>>
+	children: Vec<StrTree>
 }
 
-impl StrTree<'_> {
+impl StrTree {
 	pub fn init() -> Self {
 		return Self{
-			data:None, 
+			data: None, 
 			nb_letters: 0, 
 			is_word: false, 
-			parent: None, 
 			children: Vec::new()};
 	}
+
+	//// TO REMOVE
+	pub fn check_child(&self, c: char) -> bool {
+		for child in &self.children {
+			if child.data == Some(c) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	fn get_child_idx(&self, c: char) -> Option<usize> {
+		for i in 0..self.children.len() {
+			if self.children[i].data == Some(c) {
+				return Some(i);
+			}
+		}
+		return None;
+	}
+
+	pub fn get_child<'a>(&'a self, c: char) -> Option<&'a StrTree> {
+		if let Some(i) = self.get_child_idx(c) {
+			return Some(&self.children[i]);
+		} else {
+			return None;
+		}
+	}
+	fn get_child_mut<'a>(&'a mut self, c: char) -> Option<&'a mut StrTree> {
+		if let Some(i) = self.get_child_idx(c) {
+			return Some(&mut self.children[i]);
+		} else {
+			return None;
+		}
+	}
+
+	fn get_or_make_child(&mut self, c:char) -> &mut StrTree {
+		if let Some(idx) = self.get_child_idx(c) {
+			return &mut self.children[idx];
+		} else {
+			return self.add_child(c);
+		}
+	}
+
+	fn add_child<'a>(&'a mut self, c: char) -> &'a mut StrTree {
+		println!("Creating child {} for parent {:?}", c, self.data);
+		let new_tree = StrTree{
+			data: Some(c),
+			nb_letters: self.nb_letters + 1,
+			is_word: false,
+			children: Vec::new()
+		};
+		self.children.push(new_tree);
+		return self.children.last_mut().unwrap();
+	}
+
+	pub fn add_word(&mut self, word: &str) {
+		let mut letter_idx: usize = 0;
+		let mut node = self;
+
+		while let Some(c) = word.chars().nth(letter_idx) {
+			node = node.get_or_make_child(c);
+			letter_idx += 1;
+		}
+		node.is_word = true;
+	}
 }
+

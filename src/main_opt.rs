ed03d7978b7f4b4ee48e80c30620cd7346fcc4b9
twiby <t.balenbois@gmail.com>
2022-12-28 15:51:48 +0100

    change main to reflect experiments with profiling

--- src/main_opt.rs
+++ src/main_opt.rs
@@ -1,50 +1,18 @@
 #![allow(non_snake_case)]
 
 mod str_tree;
-use str_tree::Dictionnary;
 
 mod board;
 
 mod constraints;
 
 mod solver;
-use solver::{WithoutTimer, WithTimer};
+use solver::{WithTimer};
 
 fn main() {
-	let tree = match str_tree::build_dict_from_file("../pyScrabble/scrabbleWords.txt") {
-		Err(e) => {println!("File not read: {e:?}"); None},
-		Ok(tree) => {
-			println!("{0:?}", tree.get_anagrams("catastrophe", Some(vec![2,3]), None, None));
-			println!("{0:?}", tree.get_anagrams("a00", None, None, None).len());
-			println!("{0:?}", tree.get_anagrams("a00", Some(vec![2]), None, None).len());
-			println!("{0:?}", tree.get_anagrams("a00", Some(vec![3]), None, None).len());
-			println!("{0:?}", tree.is_word("njklhjkl"));
-			println!("{0:?}", tree.is_word("ud"));
-			println!("{0:?}", tree.is_word("woh"));
-
-			println!("{0:?}", tree.get_anagrams("arbe", None, Some(vec![(3, 'r')]), None));
-			println!("{0:?}", tree.get_anagrams("rbr", None, Some(vec![(0, 'a'), (4, 'e')]), None));
-			Some(tree)			
-		}
-	};
-	let tree_ref = &tree.unwrap();
+	let tree = str_tree::build_dict_from_file("../pyScrabble/scrabbleWords.txt").unwrap();
 
 	let mut str_board = "".to_string();
-	// str_board.push_str("6__2___6___2__6");
-	// str_board.push_str("_5___3___3___5_");
-	// str_board.push_str("__5___2_2___5__");
-	// str_board.push_str("2__5___2___5__2");
-	// str_board.push_str("____5_____5____");
-	// str_board.push_str("_3___3___3___3_");
-	// str_board.push_str("__2___2_2___2__");
-	// str_board.push_str("6__2___5___2__6");
-	// str_board.push_str("__2___2_2___2__");
-	// str_board.push_str("_3___3___3___3_");
-	// str_board.push_str("____5_____5____");
-	// str_board.push_str("2__5___2___5__2");
-	// str_board.push_str("__5___2_2___5__");
-	// str_board.push_str("_5___3___3___5_");
-	// str_board.push_str("6__2___6___2__6");
 	str_board.push_str("6__2___6___2__6");
 	str_board.push_str("_5___3___3___5_");
 	str_board.push_str("__5___2_2___5__");
@@ -62,11 +30,13 @@ fn main() {
 	str_board.push_str("6__2___6___2__6");
 
 	let board = board::deserialize(&str_board).expect("Error when deserializing board message");
-	println!("{:?}", solver::find_best_word::<WithoutTimer, _, _>("systeme", &board, tree_ref));
 
-	use std::time::Instant;
-	let now = Instant::now();
-	println!("{:?}", solver::find_best_word::<WithTimer, _, _>("syste00", &board, tree_ref));
-	let elapsed = now.elapsed();
-	println!("Elapsed: {:.2?}", elapsed);
+	let mut duration = std::time::Instant::now().elapsed();
+	for n in 0..50 {
+		use std::time::Instant;
+		let now = Instant::now();
+		println!("{:?}", solver::find_best_word::<WithTimer, _, _>("syste00", &board, &tree));
+		duration += now.elapsed();
+		println!("Elapsed: {:.2?}\n", duration/(n+1));
+	}
 }
